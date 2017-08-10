9c0108354ca0f22200e7f6bf75659a3d1855d08b
Greg doud <gpdoud@gmail.com>
2017-08-10 16:03:33 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$().ready(function() {
+
+	$("nav").append(
+		"<ul>"
+			+"<li title='PRS'><a href='index.html'>PRS</a></li>"
+			+"<li title='Home page'><a href='index.html'>Home</a></li>"
+			+"<li title='About page'><a href='about.html'>About</a></li>"
+		+"</ul>"
+	);
+
+}); // end ready
