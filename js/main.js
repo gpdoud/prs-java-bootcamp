77e3a358993409ad569abfe5e9e198ce75de8926
Greg doud <gpdoud@gmail.com>
2017-08-11 11:55:15 -0400

    getting user list from ajax server

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $().ready(function() {
 		"<ul>"
 			+"<li title='PRS'><a href='index.html'>PRS</a></li>"
 			+"<li title='Home page'><a href='index.html'>Home</a></li>"
+			+"<li title='Users'><a href='user-list.html'>Users</a></li>"
 			+"<li title='About page'><a href='about.html'>About</a></li>"
 		+"</ul>"
 	);
